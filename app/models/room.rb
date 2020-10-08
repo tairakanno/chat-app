54dbed9bbcde0c078b1437b337e03ceac7922ce6
tairakanno <tairakannotaira@gmail.com>
2020-10-08 11:04:04 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
-  has_many:room_user
-  has_many:users, throught: :room_user
+  has_many :room_users
+  has_many :users, through: :room_users
+  validates :name, presence: true
 end
